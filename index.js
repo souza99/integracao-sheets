7a03a1c163746cab0f62409313d4d577945c30ca
souza99 <jpfsouza99@gmail.com>
2022-08-31 01:24:18 -0300

    Iniciando projeto

---
+++ index.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const {google } = require("googleapis");
+const HTTP_PORT = process.env.HTTP_PORT || 3001;
+
+const app = express();
+
+async function getAuthSheets () {
+
+    //guarda as credenciais do google
+    //scopes == link da api
+    const auth = new google.auth.GoogleAuth({
+        keyFile: "credentials.json",
+        scopes: "https://www.googleapis.com/auth/spreadsheets"
+    });
+
+    //cliente da autenticação
+    const client = await auth.getClient()
+
+    //conexão com googlesheets
+    const googleSheets = google.sheets({
+        version: "v4",
+        auth: client,
+    });
+
+    //parametro esperado pelos gets da função
+    const spreadsheetId = "1Fa_NzZz8BTs7EbIYn0knQDxK_veCABefpJ0HvAweGnU";
+
+    return {
+        auth,
+        client,
+        googleSheets,
+        spreadsheetId,
+    };
+}
+
+app.get("/metadata", async (req, res) => {
+
+    const { googleSheets, auth, spreadsheetId } = await getAuthSheets();
+
+    const metadata = await googleSheets.spreadsheets.get({
+        auth,
+        spreadsheetId,
+    });
+
+    res.send(metadata);
+
+});
+
+//pega os alores das resposta da planilha
+app.get("/getRows", async (req, res) => {
+    const { googleSheets, auth, spreadsheetId } = await getAuthSheets();
+
+    const getRows = await googleSheets.spreadsheets.values.get({
+        auth,
+        spreadsheetId,
+        range: "Página2",
+        valueRenderOption: "UNFORMATTED_VALUE",
+    })
+
+    res.send(getRows.data);
+});
+
+app.listen(HTTP_PORT, () => console.log(`Ouvindo na porta ${HTTP_PORT}`));
