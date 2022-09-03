9ff0c1c39eaa76c61b666cf05450057bb8459f43
souza99 <jpfsouza99@gmail.com>
2022-09-03 01:46:51 -0300

    Realizando request para o hubSpot

--- services/GoogleSheetsService.js
+++ services/GoogleSheetsService.js
@@ -1,4 +1,5 @@
 const { google } = require("googleapis");
+const axios = require("axios");
 
 async function getAuthSheets() {
   //guarda as credenciais do google
@@ -35,41 +36,131 @@ async function GoogleSheetsService() {
   let listafinal = [];
   let pagina = 1;
 
-    while (rodaRequest) {
-        try {
+  while (rodaRequest) {
+    try {
 
-            const getRows = await googleSheets.spreadsheets.values.get({
-                auth,
-                spreadsheetId,
-                range: `Página${pagina}!A2:F1000`,
-                valueRenderOption: "UNFORMATTED_VALUE",
-              });
-               
-                listafinal.push(...getRows.data.values);
-                pagina += 1;
+      const getRows = await googleSheets.spreadsheets.values.get({
+        auth,
+        spreadsheetId,
+        range: `Página${pagina}!A2:F1000`,
+        valueRenderOption: "UNFORMATTED_VALUE",
+      });
 
-        }catch(e){
-            rodaRequest = false;
-        }
+      listafinal.push(...getRows.data.values);
+      pagina += 1;
+
+    } catch (e) {
+      rodaRequest = false;
+    }
+  }
+
+  const novaLista = [];
+
+  for (const i of listafinal) {
+
+    if ((String(i[2]).indexOf('@') > -1) &&
+      !(String(i[2].indexOf('@gmail')) > -1) &&
+      !(String(i[2].indexOf('@hotmail')) > -1) &&
+      !(String(i[2].indexOf('@outlook')) > -1)) {
+      console.log('exibindo valor ', i[2]);
+      novaLista.push(i);
     }
 
-    const novaLista = [];
+  }
 
-    for(const i of listafinal) { 
+  //console.log(novaLista);
 
-      if ((String(i[2]).indexOf('@') > -1) &&
-       !(String(i[2].indexOf('@gmail')) > -1) &&
-        !(String(i[2].indexOf('@hotmail')) > -1) &&
-         !(String(i[2].indexOf('@outlook')) > -1) ) {
-        console.log('exibindo valor ', i[2]);
-        novaLista.push(i);
-      }
-
-     
-    }
-
-    console.log(novaLista);
-    return novaLista;
+  const retornoHubContacts = await postHubSportCmr();
+  console.log(retornoHubContacts);
+  return novaLista;
 }
+
+// const postHubSportCmr = () => {
+
+//   ////informa o token para validasção
+// const ACCESS_TOKEN = "pat-na1-fcb92bc0-ac4a-4b46-808b-2d04a1d552a9"
+
+//   const options = {
+//     method: 'GET',
+//     url: 'https://api.hubapi.com/crm/v3/objects/contacts',
+//     headers: { Authorization: `Bearer ${ACCESS_TOKEN}` }
+//   };
+
+//   return axios.request(options).then(function (response) {
+//     console.log(response.data);
+//   }).catch(function (error) {
+//     console.error(error);
+//   });
+
+// }
+
+//POST
+
+
+const postHubSportCmr = () => {
+
+  // const ACCESS_TOKEN = "pat-na1-fcb92bc0-ac4a-4b46-808b-2d04a1d552a9";
+  // var request = require("request");
+
+  // var options = {
+  //   method: 'POST',
+  //   url: 'https://api.hubapi.com/contacts/v1/contact/',
+  //   qs: { hapikey: 'demo' },
+  //   headers:
+  //   {
+  //     'Content-Type': 'application/json',
+  //     'Authorization': `Bearer ${ACCESS_TOKEN}`,
+  //   },
+  // body:
+  //   {
+  //     properties:
+  //       [{ property: 'email', value: {} },
+  //       { property: 'firstname', value: {} },
+  //       { property: 'website', value: {} },
+  //       { property: 'company', value: {} },
+  //       { property: 'phone', value: {} },]
+  //   },
+  //   json: true
+  // };
+
+  // request(options, function (error, response, body) {
+  //   if (error) throw new Error(error);
+
+  //   console.log(body);
+  // });
+
+  const ACCESS_TOKEN = "pat-na1-fcb92bc0-ac4a-4b46-808b-2d04a1d552a9";
+
+  const options = {
+    method: 'POST',
+    url: 'https://api.hubapi.com/contacts/v1/contact/',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${ACCESS_TOKEN} `,
+    },
+    data: {
+      properties: [
+        { property: 'email', value: 'testingapis@hubspot.com' },
+        { property: 'firstname', value: 'Adrian' },
+        { property: 'lastname', value: 'Mott' },
+        { property: 'website', value: 'http://hubspot.com' },
+        { property: 'company', value: 'HubSpot' },
+        { property: 'phone', value: '555-122-2323' },
+        { property: 'address', value: '25 First Street' },
+        { property: 'city', value: 'Cambridge' },
+        { property: 'state', value: 'MA' },
+        { property: 'zip', value: '02139' },
+      ]
+    }
+  };
+
+  return axios.request(options).then(function (response) {
+    console.log(response.data);
+  }).catch(function (error) {
+    console.error(error);
+  });
+
+}
+
 
 module.exports = GoogleSheetsService;
